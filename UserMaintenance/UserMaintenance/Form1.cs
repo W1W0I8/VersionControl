707552a31ebcb641677033a497453d85857b7af6
Kardos Keve <geza.kardos@stud.uni-corvinus.hu>
2019-11-30 11:49:35 +0100

    savetofile commit

--- UserMaintenance/UserMaintenance/Form1.cs
+++ UserMaintenance/UserMaintenance/Form1.cs
@@ -3,6 +3,7 @@ using System.Collections.Generic;
 using System.ComponentModel;
 using System.Data;
 using System.Drawing;
+using System.IO;
 using System.Linq;
 using System.Text;
 using System.Threading.Tasks;
@@ -20,6 +21,7 @@ namespace UserMaintenance
 
             label1.Text = Resource1.FullName;            
             button1.Text = Resource1.Add;
+            button2.Text = Resource1.SaveToFile;
 
             listBox1.DataSource = users;
             listBox1.ValueMember = "ID";
@@ -34,6 +36,20 @@ namespace UserMaintenance
                 
             };
             users.Add(u);
+        }
+
+        private void button2_Click(object sender, EventArgs e)
+        {
+            SaveFileDialog sfd = new SaveFileDialog();
+
+            if (sfd.ShowDialog() != DialogResult.OK) return;
+
+            StreamWriter sw = new StreamWriter(sfd.FileName, false, Encoding.UTF8);
+            foreach (User item in users)
+            {
+                sw.WriteLine($"{item.ID};{item.FullName}");
+            }
+            sw.Close();
         }
     }
 }
