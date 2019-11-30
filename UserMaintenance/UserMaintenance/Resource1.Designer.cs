16e09fb5e1554a4f539fbca0aa43727143a11144
Kardos Keve <geza.kardos@stud.uni-corvinus.hu>
2019-11-30 12:54:05 +0100

    törlés commit javítása

--- UserMaintenance/UserMaintenance/Resource1.Designer.cs
+++ UserMaintenance/UserMaintenance/Resource1.Designer.cs
@@ -70,6 +70,15 @@ namespace UserMaintenance {
         }
         
         /// <summary>
+        ///   Looks up a localized string similar to A kiválasztott fájl törlése.
+        /// </summary>
+        internal static string DeleteSelectedItem {
+            get {
+                return ResourceManager.GetString("DeleteSelectedItem", resourceCulture);
+            }
+        }
+        
+        /// <summary>
         ///   Looks up a localized string similar to Teljes név.
         /// </summary>
         internal static string FullName {
